3c61ff0c756a830c84afb53148d3f3948c502751
Valeria <valeriakirkbride@gmail.com>
2023-08-31 00:18:11 -0600

    looping

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(recipients, msg) {
+    const emptyArray = [];
+    for(let i = 0; i < recipients.length; i++) {
+        emptyArray.push(`Thank you, ${recipients[i]}, for the wonderful ${msg} gift!`);
+    }
+    return emptyArray
+}
+
+function countDown(int) {
+    while (int <= 11 && int >= 0) {
+        console.log(int--)
+    }  
+}
